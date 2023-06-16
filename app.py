1189e82ce9695d11dd1d82d30c5cfcea2180211c
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-06-15 20:24:50 -0500

    search bar added and starting page designed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,6 +8,12 @@ CORS(app)
 @app.route("/api/hello", methods=["GET"])
 def hello():
     return jsonify({"message": "This project was set up by your man - Imad"})
+
+
+@app.route("/weather", methods=["GET"])
+def get_weather():
+    location = request.args.get("location", default="*", type=str)
+    return jsonify({"location": location})
 
 
 if __name__ == "__main__":
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,48 @@
-import logo from "./logo.svg";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+  const searchWeather = () => {
+    fetch("/weather?location=" + encodeURIComponent(searchQuery))
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.location);
+      });
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{message}</p>
-      </header>
+      <Container className="vh-100 d-flex flex-column align-items-center justify-content-center">
+        <h1 className="app-title mb-4">Welcome to WeatherView</h1>
+        <p className="app-tagline mb-4">
+          Your one-stop destination for accurate city-wide weather updates
+        </p>
+        <Form className="w-50">
+          <Row>
+            <Col md={8}>
+              <Form.Control
+                className="mb-2"
+                type="text"
+                value={searchQuery}
+                placeholder="Enter city name, Zipcode or Country name"
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+            </Col>
+            <Col
+              md={4}
+              className="d-flex align-items-center justify-content-center"
+            >
+              <Button className="search-btn" onClick={searchWeather}>
+                Search
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
     </div>
   );
 }
