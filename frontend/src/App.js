1cb9f69405a0d9e00655a1cd9016f64db837d161
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-06-19 09:43:24 -0500

    API calls minimized + temp chart added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -12,12 +12,46 @@ import {
 } from "react-router-dom";
 import WeatherDashboard from "./components/WeatherDashboard";
 
+// Create the context
+export const WeatherDataContext = createContext();
+
 function App() {
+  // const [weatherData, setWeatherData] = useState(null);
+  const [coordinates, setCoordinates] = useState({
+    latitude: null,
+    longitude: null,
+  });
+  const [error, setError] = useState(null);
+
   const location = useLocation();
   const isHomePage = location.pathname === "/";
+
+  // useEffect(() => {
+  //   if (coordinates.latitude && coordinates.longitude) {
+  //     fetchWeatherData(coordinates.latitude, coordinates.longitude).then(
+  //       (data) => {
+  //         if (data.error) {
+  //           setError(data.error);
+  //         } else {
+  //           setWeatherData(data);
+  //         }
+  //       }
+  //     );
+  //   }
+  // }, [coordinates]);
+
+  // const fetchWeatherData = async (latitude, longitude) => {
+  //   const response = await fetch(
+  //     `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&start_date=2023-06-10&end_date=2023-06-24&timezone=America%2FChicago`
+  //   );
+  //   const weatherData = await response.json();
+  //   return weatherData;
+  // };
+
   return (
-    <div className="App">
+    <WeatherDataContext.Provider value={{ coordinates, setCoordinates }}>
       <Header />
+      {error && <div>Error: {error}</div>}
       {isHomePage && (
         <Container className="vh-100 d-flex flex-column align-items-center justify-content-center">
           <h1 className="app-title mb-4">Welcome to WeatherView</h1>
@@ -30,11 +64,11 @@ function App() {
       {!isHomePage && (
         <div>
           <Routes>
-            <Route path="/dashboard/:location" element={<WeatherDashboard />} />
+            <Route path="/dashboard" element={<WeatherDashboard />} />{" "}
           </Routes>
         </div>
       )}
-    </div>
+    </WeatherDataContext.Provider>
   );
 }
 
