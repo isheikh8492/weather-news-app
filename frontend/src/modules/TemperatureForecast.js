20555ad4d4e1eac88c2c2441efb66fc9e73d99f5
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-06-27 19:10:05 -0500

    added weathercards scrollable

--- frontend/src/modules/TemperatureForecast.js
+++ frontend/src/modules/TemperatureForecast.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "../css/modules/TemperatureForecast.css";
 
 const TemperatureForecast = () => {
+  const data = new Array(64).fill(null); // Dummy data
+  const itemsPerSlide = 8; // Items per slide
+
+  const slides = data.reduce((acc, _, i) => {
+    if (i % itemsPerSlide === 0) acc.push(data.slice(i, i + itemsPerSlide));
+    return acc;
+  }, []);
+
   return (
     <div className="summary-container">
-      <div className="summary-box">Temperature Forecast</div>
+      <div className="summary-box">
+        <Carousel
+          showArrows
+          showStatus={false}
+          showThumbs={false}
+          showIndicators={false}
+          dynamicHeight
+        >
+          {slides.map((slide, i) => (
+            <div key={i} className="weatherCardContainer">
+              {slide.map((item, index) => (
+                <div key={index} className="weatherCard">
+                  <span className="weatherCard-text">
+                    Weather Card {index + 1 + i * itemsPerSlide}
+                  </span>
+                </div>
+              ))}
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </div>
   );
 };
