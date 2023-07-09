4f82cdb0d95f61455d5975ba368458a989b3b329
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-07-09 16:07:56 -0500

    frontend updates/fixes

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -10,25 +10,27 @@ import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <Link to="/dashboard" className="sidebar-item">
-        <button className="sidebar-item">
-          <FontAwesomeIcon icon={faCloud} />
-          <span className="sidebar-text">Weather Conditions</span>
-        </button>
-      </Link>
-      <Link to="/social" className="sidebar-item">
-        <button className="sidebar-item">
-          <FontAwesomeIcon icon={faNewspaper} />
-          <span className="sidebar-text">Social Feed</span>
-        </button>
-      </Link>
-      <Link to="/settings" className="sidebar-item">
-        <button className="sidebar-item">
-          <FontAwesomeIcon icon={faGear} />
-          <span className="sidebar-text">Settings</span>
-        </button>
-      </Link>
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <Link to="/dashboard" className="sidebar-item">
+          <button className="sidebar-item" title="Weather Conditions">
+            <FontAwesomeIcon icon={faCloud} />
+            <span className="sidebar-text">Weather Conditions</span>
+          </button>
+        </Link>
+        <Link to="/social" className="sidebar-item">
+          <button className="sidebar-item" title="Social Feed">
+            <FontAwesomeIcon icon={faNewspaper} />
+            <span className="sidebar-text">Social Feed</span>
+          </button>
+        </Link>
+        <Link to="/settings" className="sidebar-item">
+          <button className="sidebar-item" title="Settings">
+            <FontAwesomeIcon icon={faGear} />
+            <span className="sidebar-text">Settings</span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
