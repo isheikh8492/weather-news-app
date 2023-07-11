6550a070ed73b37723685b08287cfdc633e622c1
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2023-07-11 09:42:03 -0500

    heroku deploy fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 import os
 
-app = Flask(__name__, static_folder="./frontend/build")
+app = Flask(__name__, static_folder="./frontend/build", static_url_path="/")
 
 CORS(app)
 load_dotenv()
